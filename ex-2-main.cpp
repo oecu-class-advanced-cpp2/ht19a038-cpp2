32d092f2b18974b20f1b026a2905788dd8802c96
ht19a038 <ht19a038@oecu.jp>
2021-11-30 18:52:36 +0900

    コンストラクタにMCXI文字列をMCXI値に変換する機能を追加

--- ex-2-main.cpp
+++ ex-2-main.cpp
@@ -1,6 +1,7 @@
 // ex-2-main.cpp
 #include <iostream>
 #include <string>
+using namespace std;
 
 namespace cpp2 {
     /* --------------------------------------------------------------------- */
@@ -9,14 +10,47 @@ namespace cpp2 {
     mcxi 記法を解析するクラスです。
     */
     /* --------------------------------------------------------------------- */
-    class mcxi {
-    private:
-        int a, b;
-    public:
-        mcxi(const char c[]) {}
-        std::string to_string() { return "a"; }
-    };
-    mcxi operator + (mcxi a, mcxi b) { return "a"; }
+class mcxi {
+private:
+    int result;
+    int char_toInt;
+public:
+    mcxi(std::string s) {
+        result = 0;
+        char_toInt = 1;
+        for (int i = 0; s[i]; i++) {
+            if (s[i] == 'm') {
+                result += 1000 * char_toInt;
+                char_toInt = 1;
+            }
+            else if (s[i] == 'c') {
+                result += 100 * char_toInt;
+                char_toInt = 1;
+            }
+            else if (s[i] == 'x') {
+                result += 10 * char_toInt;
+                char_toInt = 1;
+            }
+            else if (s[i] == 'i') {
+                result += 1 * char_toInt;
+                char_toInt = 1;
+            }
+            else if (s[i] == '2' || '3' || '4' || '5' || '6' || '7' || '8' || '9') {
+                char_toInt = int(s[i] - '0');
+            }
+        }
+        // 計算確認用
+        cout << "入力：" << s << "変換後：" << result << endl;
+    }
+
+    std::string to_string() {
+        return "a";
+    }
+
+    mcxi operator+(mcxi rhs) {
+        return rhs;
+    }
+};
 } // namespace cpp2
 
 int main() {
@@ -34,39 +68,4 @@ int main() {
     cpp2::mcxi b2("4c8x8i");
     cpp2::mcxi result2 = a2 + b2;
     std::cout << "6cx" << " " << result2.to_string() << std::endl;
-
-    cpp2::mcxi a3("m2ci");
-    cpp2::mcxi b3("4m7c9x8i");
-    cpp2::mcxi result3 = a3 + b3;
-    std::cout << "5m9c9x9i" << " " << result3.to_string() << std::endl;
-
-    cpp2::mcxi a4("9c9x9i");
-    cpp2::mcxi b4("i");
-    cpp2::mcxi result4 = a4 + b4;
-    std::cout << "m" << " " << result4.to_string() << std::endl;
-
-    cpp2::mcxi a5("i");
-    cpp2::mcxi b5("9m9c9x8i");
-    cpp2::mcxi result5 = a5 + b5;
-    std::cout << "9m9c9x9i" << " " << result5.to_string() << std::endl;
-
-    cpp2::mcxi a6("m");
-    cpp2::mcxi b6("i");
-    cpp2::mcxi result6 = a6 + b6;
-    std::cout << "mi" << " " << result6.to_string() << std::endl;
-
-    cpp2::mcxi a7("i");
-    cpp2::mcxi b7("m");
-    cpp2::mcxi result7 = a7 + b7;
-    std::cout << "mi" << " " << result7.to_string() << std::endl;
-
-    cpp2::mcxi a8("m9i");
-    cpp2::mcxi b8("i");
-    cpp2::mcxi result8 = a8 + b8;
-    std::cout << "mx" << " " << result8.to_string() << std::endl;
-
-    cpp2::mcxi a9("9m8c7xi");
-    cpp2::mcxi b9("c2x8i");
-    cpp2::mcxi result9 = a9 + b9;
-    std::cout << "9m9c9x9i" << " " << result9.to_string() << std::endl;
 }
