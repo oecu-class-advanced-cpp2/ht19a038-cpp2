f42683b1ff8b72db9450f8c99715df442aa3163b
ht19a038 <ht19a038@oecu.jp>
2021-12-14 14:13:49 +0900

    書き方を授業内でのものに変更

--- ex-2-main.cpp
+++ ex-2-main.cpp
@@ -1,82 +1,147 @@
-// ex-2-main.cpp
 #include <iostream>
 #include <string>
-using namespace std;
+#include <algorithm>
 
 namespace cpp2 {
     /* --------------------------------------------------------------------- */
     /*
     mcxi
+
     mcxi 記法を解析するクラスです。
     */
     /* --------------------------------------------------------------------- */
     class mcxi {
-    private:
-        int result;
-        int char_toInt;
     public:
-        mcxi(const std::string s) {
-            result = 0;
-            char_toInt = 1;
-            for (int i = 0; s[i]; i++) {
-                if (s[i] == 'm') {
-                    result += 1000 * char_toInt;
-                    char_toInt = 1;
+        /* ----------------------------------------------------------------- */
+        /*
+        mcxi
+
+        指定された文字列を解析して、オブジェクトを初期化します。
+        以下の場合には例外が創出されます。
+
+        1. [2-9,m,c,x,i] 以外の文字が出現した場合
+        2. 2 文字続けて数字 (2-9) が出現した場合
+        3. m, c, x, i がこの順序で出現しなかった場合
+        ただし、例えば mx のように、特定の文字をスキップする事は許容
+        されます。
+        */
+        /* ----------------------------------------------------------------- */
+        mcxi(const std::string& s) : value_(0) {
+            int num = 0;
+            for (auto pos = s.begin(); pos != s.end(); ++pos) {
+                //*pos は、char ! char ってことが分かってたら、もっと簡単にできるのでは？？
+                if (*pos >= '2' && *pos <= '9') {
+                    num = *pos - '0';
                 }
-                else if (s[i] == 'c') {
-                    result += 100 * char_toInt;
-                    char_toInt = 1;
-                }
-                else if (s[i] == 'x') {
-                    result += 10 * char_toInt;
-                    char_toInt = 1;
-                }
-                else if (s[i] == 'i') {
-                    result += 1 * char_toInt;
-                    char_toInt = 1;
-                }
-                // charの数字をintに変換
                 else {
-                    char_toInt = int(s[i] - '0');
+                    int u = unit(*pos);
+                    value_ += std::max(num, 1) * u;
+                    num = 0;
                 }
             }
-            // 計算確認用
-            cout << "入力：" << s << "変換後：" << result << endl;
         }
 
-        std::string to_string() {
-            return "a";
-        }
+        /* ----------------------------------------------------------------- */
+        /*
+        operator+
 
-        mcxi operator +(const mcxi& rhs) {
+        2 つのオブジェクトの加算結果を取得します。
+        */
+        /* ----------------------------------------------------------------- */
+        mcxi operator+(const mcxi& rhs) {
             mcxi mcxi(rhs);
-            mcxi.result = result + rhs.result;
+            mcxi.value_ = value_ + rhs.value_;
             return mcxi;
         }
+
+        /* ----------------------------------------------------------------- */
+        /*
+        to_string
+
+        現在の値を mcxi 記法に変換します。
+        */
+        /* ----------------------------------------------------------------- */
+        std::string to_string() const {
+            return "XXX";
+        }
+
+        void debug_mcxi() {
+            std::cout << "value_: " << value_ << std::endl;
+        }
+
         // 足し算確認用
         int get() {
-            return result;
+            return value_;
         }
+    private:
+        int unit(char c) {
+            switch (c) {
+            case 'm': return 1000;
+            case 'c': return  100;
+            case 'x': return   10;
+            case 'i': return    1;
+            }
+            return -1;
+        }
+
+    private:
+        int value_;
     };
-} // namespace cpp2
+}
 
 int main() {
     cpp2::mcxi a0("xi");
+    a0.debug_mcxi();
     cpp2::mcxi b0("x9i");
-    cpp2::mcxi result0 = a0 + b0;
-    // 足し算確認
-    std::cout << "足し算結果：" << result0.get() << std::endl;
+    b0.debug_mcxi();
+    auto result0 = a0 + b0;
+    std::cout << "足し算結果 " << result0.get() << std::endl;
     std::cout << "3x" << " " << result0.to_string() << std::endl;
 
+    /*
     cpp2::mcxi a1("i");
     cpp2::mcxi b1("9i");
-    cpp2::mcxi result1 = a1 + b1;
-    std::cout << "足し算結果：" << result1.get() << std::endl;
+    auto result1 = a1 + b1;
     std::cout << "x" << " " << result1.to_string() << std::endl;
 
     cpp2::mcxi a2("c2x2i");
     cpp2::mcxi b2("4c8x8i");
-    cpp2::mcxi result2 = a2 + b2;
-    std::cout << "足し算結果：" << result2.get() << std::endl;
+    auto result2 = a2 + b2;
     std::cout << "6cx" << " " << result2.to_string() << std::endl;
+
+    cpp2::mcxi a3("m2ci");
+    cpp2::mcxi b3("4m7c9x8i");
+    auto result3 = a3 + b3;
+    std::cout << "5m9c9x9i" << " " << result3.to_string() << std::endl;
+
+    cpp2::mcxi a4("9c9x9i");
+    cpp2::mcxi b4("i");
+    auto result4 = a4 + b4;
+    std::cout << "m" << " " << result4.to_string() << std::endl;
+
+    cpp2::mcxi a5("i");
+    cpp2::mcxi b5("9m9c9x8i");
+    auto result5 = a5 + b5;
+    std::cout << "9m9c9x9i" << " " << result5.to_string() << std::endl;
+
+    cpp2::mcxi a6("m");
+    cpp2::mcxi b6("i");
+    auto result6 = a6 + b6;
+    std::cout << "mi" << " " << result6.to_string() << std::endl;
+
+    cpp2::mcxi a7("i");
+    cpp2::mcxi b7("m");
+    auto result7 = a7 + b7;
+    std::cout << "mi" << " " << result7.to_string() << std::endl;
+
+    cpp2::mcxi a8("m9i");
+    cpp2::mcxi b8("i");
+    auto result8 = a8 + b8;
+    std::cout << "mx" << " " << result8.to_string() << std::endl;
+
+    cpp2::mcxi a9("9m8c7xi");
+    cpp2::mcxi b9("c2x8i");
+    auto result9 = a9 + b9;
+    std::cout << "9m9c9x9i" << " " << result9.to_string() << std::endl;
+    */
 }
